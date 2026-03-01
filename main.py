2b81beef15a6b88dc4d83fbdcd6540407f80f290
ApoorvaBhatD <bhatapoorva24@gmail.com>
2026-03-01 09:43:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -88,5 +88,5 @@ def delete_item():
     return jsonify({"success": True})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
